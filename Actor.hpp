b11bd8fbb5b1078bae3234a031a9e24bfe2205d7
ccnelson <46001311+ccnelson@users.noreply.github.com>
2021-03-21 10:41:19 +0000

    Initial commit

---
+++ Actor.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "libtcod.hpp"
+
+class Actor
+{
+public:
+	int x, y;
+	int ch;
+	TCODColor col;
+
+	Actor(int x, int y, int ch, const TCODColor& col);
+	void render() const;
+};
---
+++ Engine.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "libtcod.hpp"
+
+class Actor;
+class Map;
+
+class Engine
+{
+public:
+	TCODList<Actor*> actors;
+	Actor* player;
+	Map* map;
+
+	Engine();
+	~Engine();
+	void update();
+	void render();
+};
+
+extern Engine engine;
